20534d4f84a5f21dbfe5fc56b8ddfd49d8163177
TheDao032 <nthedao2705@gmail.com>
2021-04-21 01:15:25 +0700

    update

--- Dictionary/src/main/java/slang_dictionary/Slang.java
+++ Dictionary/src/main/java/slang_dictionary/Slang.java
@@ -8,11 +8,8 @@ package slang_dictionary;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.*;
-import java.util.Map.Entry;
-import java.util.function.Consumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Stream;
 
 /**
  *
@@ -20,10 +17,12 @@ import java.util.stream.Stream;
  */
 public class Slang {
     public HashMap<String, String> slang;
+    public HashMap<String, String> backup;
     public ArrayList<String> slang_history;
 
     public Slang() {
         this.slang = new HashMap<>();
+        this.backup = new HashMap<>();
         this.slang_history = new ArrayList<>();
         String path = "slang.txt";
         String line;
@@ -32,6 +31,7 @@ public class Slang {
             while ((line = reader.readLine()) != null) {
                 String[] values = line.split("`");
                 this.slang.put(values[0], values[1]);
+                this.backup.put(values[0], values[1]);
             }
         } catch(IOException e) {
             System.err.println(e.getMessage());
@@ -44,34 +44,30 @@ public class Slang {
         String path = "slang.txt";
         BufferedWriter writer = new BufferedWriter(new FileWriter(path));
         String item = key + '`' + value;
-        this.slang.keySet().stream().filter(i -> (i.equals(key))).forEachOrdered(new Consumer<String>() {
-            @Override
-            public void accept(String i) {
-                Scanner scan = new Scanner(System.in);
-                System.out.println("There Aready Has This Key Word, Do You Want To Override It Or Create A New One: O OR C 'O: Override', 'C: Create A New One'");
-                String Choice = scan.nextLine();
-                if ("C".equals(Choice)) {
-                    byte[] array = new byte[7]; // length is bounded by 7
-                    new Random().nextBytes(array);
-                    String generatedString = new String(array, Charset.forName("UTF-8"));
-                    String result = key + generatedString;
-                    Slang.this.slang.put(result, value);
+        this.slang.keySet().stream().filter(i -> (i.equals(key))).forEachOrdered((String i) -> {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("There Aready Has This Key Word, Do You Want To Override It Or Create A New One: O OR C 'O: Override', 'C: Create A New One'");
+            String Choice = scan.nextLine();
+            if ("C".equals(Choice)) {
+                byte[] array = new byte[7]; // length is bounded by 7
+                new Random().nextBytes(array);
+                String generatedString = new String(array, Charset.forName("UTF-8"));
+                String result = key + generatedString;
+                Slang.this.slang.put(result, value);
+                try {
+                    writer.write(item);
+                } catch (IOException ex) {
+                    Logger.getLogger(Slang.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else {
+                Slang.this.slang.put(key, value);
+                Slang.this.slang.entrySet().stream().map(m -> m.getKey() + '`' + m.getValue()).forEachOrdered(itemResult -> {
                     try {
-                        writer.write(item);
+                        writer.write(itemResult);
                     } catch (IOException ex) {
                         Logger.getLogger(Slang.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                } else {
-                    Slang.this.slang.put(key, value);
-                    for (Map.Entry m : Slang.this.slang.entrySet()) {
-                        String itemResult = m.getKey().toString() + '`' + m.getValue().toString();
-                        try {
-                            writer.write(itemResult);
-                        } catch (IOException ex) {
-                            Logger.getLogger(Slang.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    }
-                }
+                });
             }
         });
         
@@ -93,5 +89,53 @@ public class Slang {
         }).forEachOrdered(entry -> {
             System.out.println(entry.getKey());
         });
+    }
+    
+    public void historySearch() {
+        this.slang_history.forEach(item -> {
+            System.out.println(item);
+        });
+    }
+    
+    public void editSlang() {
+        Slang.this.slang.entrySet().stream().map(m -> m).forEachOrdered(itemResult -> {
+            System.out.println(itemResult.getKey() + ':' + itemResult.getValue());
+        });
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("input key: ");
+        String key = scan.nextLine();
+        scan = new Scanner(System.in);
+        System.out.println("input new definition for key: ");
+        String value = scan.nextLine();
+        this.slang.put(key, value);
+    }
+    
+    public void deleteSlang() {
+        Slang.this.slang.entrySet().stream().map(m -> m).forEachOrdered(itemResult -> {
+            System.out.println(itemResult.getKey() + ':' + itemResult.getValue());
+        });
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("input key: ");
+        String key = scan.nextLine();
+        int confirm;
+        scan = new Scanner(System.in);
+        System.out.println("confirm: input 1: yes, 0: no");
+        confirm = Integer.parseInt(scan.nextLine());
+        if (confirm == 1) {
+            this.slang.remove(key);
+        }
+    }
+    
+    public void reset() {
+        this.slang = this.backup;
+    }
+    
+    public void randomSlang() {
+        Random generator = new Random();
+        Object[] keys = this.slang.keySet().toArray();
+        Object randomKey = keys[generator.nextInt(keys.length)];
+        System.out.println("key: " + randomKey + "value: " + this.slang.get(randomKey));
     }
 }
